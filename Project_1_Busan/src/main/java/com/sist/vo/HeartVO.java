f09a10937ea0603dd22ab9040a2eebab034810b8
ARI-S2 <duswl966@gmail.com>
2024-01-04 14:36:45 +0900

    김연수(찜하기, 좋아요 VO생성)

---
+++ Project_1_Busan/src/main/java/com/sist/vo/HeartVO.java
@@ -0,0 +1,32 @@
+package com.sist.vo;
+
+public class HeartVO {
+	private int hno,no,cateno;
+	private MemberVO mvo = new MemberVO();
+	public int getHno() {
+		return hno;
+	}
+	public void setHno(int hno) {
+		this.hno = hno;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getCateno() {
+		return cateno;
+	}
+	public void setCateno(int cateno) {
+		this.cateno = cateno;
+	}
+	public MemberVO getMvo() {
+		return mvo;
+	}
+	public void setMvo(MemberVO mvo) {
+		this.mvo = mvo;
+	}
+	
+	
+}
